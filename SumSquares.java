b299a6dce929720b8cdbe21cdebb198cdb8b1f02
zzaynah <zk283@exeter.ac.uk>
2026-01-30 22:03:51 +0000

    SumSquares completed with while loop

--- SumSquares.java
+++ SumSquares.java
@@ -26,8 +26,10 @@ public class SumSquares {
         System.out.println("Sum of squares from  " + startNum + " to " + endNum + ":");
 
         int total = 0;
-        for (int i = startNum; i <= endNum; i++) {
+        int i = startNum;
+        while (i <= endNum) {
             total += (i * i);
+            i++;
         }
 
         System.out.println(total);
