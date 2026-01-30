442c156a9ad5713316dc98feb470f55e7d7c0132
zzaynah <zk283@exeter.ac.uk>
2026-01-30 10:16:42 +0000

    NumbersInSpanish completed with if - else if - else

---
+++ NumbersInSpanish.java
@@ -0,0 +1,21 @@
+public class NumbersInSpanish {
+    public static void main(String[] args) {
+       // read the first argument passed to the program
+       int number = Integer.parseInt(args[0]); 
+ 
+       // delete the line below and replace with your code
+       if (number == 1) {
+        System.out.println("uno");
+       } else if (number == 2) {
+        System.out.println("dos");
+       } else if (number == 3) {
+        System.out.println("tres");
+       } else if (number == 4) {
+        System.out.println("cuatro");
+       } else if (number == 5) {
+        System.out.println("cinco");
+       } else {
+        System.out.println("Sorry I do not know that!");
+       }
+       }
+    }
